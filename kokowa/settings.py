3ef288c31a80661b9c188526d3647f35d88f489a
Reina630 <89133502+Reina630@users.noreply.github.com>
2025-12-16 00:08:30 +0100

    fix: config PostgreSQL et ALLOWED_HOSTS

--- kokowa/settings.py
+++ kokowa/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-*-m)^6n9_e9fdem^%3092#_$sp9*$fb=#(=w7-i+61)w!th_v3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.onrender.com']
 
 
 # Application definition
